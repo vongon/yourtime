2d5c1c704fa35f6559b49464792fd2e2d2682b7d
Ryan James McGill <ryanjamesmcgill@gmail.com>
2016-08-08 11:24:16 -0700

    editing landing page and build

--- shared/components/companypage/footer.jsx
+++ shared/components/companypage/footer.jsx
@@ -7,15 +7,13 @@ var Footer = React.createClass({
                 <div className="container">
                     <div className="row">
                         <div className="col-md-4">
-                            <span className="copyright">Copyright &copy; Your Website 2014</span>
+                            <span className="copyright">Copyright &copy; YourTime 2016</span>
                         </div>
                         <div className="col-md-4">
                             <ul className="list-inline social-buttons">
                                 <li><a href="#"><i className="fa fa-twitter"></i></a>
                                 </li>
                                 <li><a href="#"><i className="fa fa-facebook"></i></a>
-                                </li>
-                                <li><a href="#"><i className="fa fa-linkedin"></i></a>
                                 </li>
                             </ul>
                         </div>
--- shared/components/companypage/sections/contact.jsx
+++ shared/components/companypage/sections/contact.jsx
@@ -8,9 +8,9 @@ var ContactSection = React.createClass({
                 <div className="row">
                     <div className="col-lg-12 text-center">
                         <h2 className="section-heading">Contact Us</h2>
-                        <h3 className="section-subheading text-muted-onblack">Have any questions? Give us a call!</h3>
-                        <h3 className="section-heading">(555)-555-5555</h3>
-                        <h3 className="section-heading">info@yourtime.life</h3>
+                        <h3 className="section-subheading text-muted-onblack">Have any questions?</h3>
+                        {/*<h3 className="section-heading">(555)-555-5555</h3>*/}
+                        <h3 className="section-heading"><a target="_blank" href="mailto:info@yourtime.life">info@yourtime.life</a></h3>
                     </div>
                 </div>
             </div>
